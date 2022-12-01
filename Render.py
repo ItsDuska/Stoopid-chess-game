87d255280a3eadc94b8ad682e6c6b8ceba3b1168
ItsDuska <Yeetboiyeswhynot@gmail.com>
2022-12-01 16:45:11 +0200

    amogus

--- Render.py
+++ Render.py
@@ -10,16 +10,16 @@ class Render:
         self.laatikot = []
         self.valitunNappulanLaatikko = pygame.Surface(
             self.näyttö.get_size(), pygame.SRCALPHA)
-        self.tausta = pygame.image.load(
-            os.path.join("ShakkiKuvat", "ShakkiLauta.png")).convert_alpha()
-        self.tausta = pygame.transform.scale(self.tausta, self.näytönKoko)
+        self.tausta = pygame.image.load(os.path.join(
+            "ShakkiKuvat", "ShakkiLauta.png")).convert_alpha()
+        self.tausta = pygame.transform.scale(
+            self.tausta, self.näyttö.get_size())
 
     def update(self, kuva):
         self.kuva = pygame.image.load(
             os.path.join("ShakkiKuvat", kuva)).convert_alpha()
         self.kuva = pygame.transform.scale(
             self.kuva, (self.ruutuKoko, self.ruutuKoko))
-        # Laattikko lista tähä eli se jossa on highlited alueet
 
     def Lisää_kuva(self, x, y, kuva, sprites):
         nappulaKuva = Kuvat((x, y), self.ruutuKoko, kuva, self.näyttö)
@@ -40,14 +40,13 @@ class Render:
     def HighlightRuudut(self, klikattu,  x, y, mahdollisetPaikat):
         if not klikattu:
             return
-        pygame.draw.rect(self.valitunNappulanLaatikko,
-                         (255, 255, 153, 128), (0, 0, self.ruutuKoko, self.ruutuKoko))
+        pygame.draw.rect(self.valitunNappulanLaatikko, (255, 255,
+                         153, 128), (0, 0, self.ruutuKoko, self.ruutuKoko))
         self.näyttö.blit(self.valitunNappulanLaatikko,
                          (x*self.ruutuKoko, y*self.ruutuKoko))
         for index, ruutu in enumerate(mahdollisetPaikat):
             self.näyttö.blit(
-                self.laatikot[index], (ruutu[0]*self.ruutuKoko,
-                                       ruutu[1]*self.ruutuKoko))
+                self.laatikot[index], (ruutu[0]*self.ruutuKoko, ruutu[1]*self.ruutuKoko))
 
     def drawBG(self):
         self.näyttö.blit(self.tausta, (0, 0))
